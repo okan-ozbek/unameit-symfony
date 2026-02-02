35a2222937d51a97ff9cb27b207ab6687860b64a
Okan Ozbek <devokan@outlook.com>
2026-02-02 15:21:31 +0100

    Crud

--- src/Controller/V1/TaskController.php
+++ src/Controller/V1/TaskController.php
@@ -2,41 +2,80 @@
 
 namespace App\Controller\V1;
 
+use App\Entity\Task;
+use App\Service\TaskService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
+    public function __construct(private readonly TaskService $taskService) {
+
+    }
+
     #[Route(path: '/v1/tasks', name: 'v1_tasks', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->json(['message' => 'Hello world'], Response::HTTP_OK);
+        $tasks = $this->taskService->fetchAll();
+
+        if ($tasks === []) {
+            return $this->json(['message' => 'No tasks found.'], Response::HTTP_NO_CONTENT);
+        }
+
+        return $this->json(['tasks' => $tasks], Response::HTTP_OK);
     }
 
     #[Route(path: '/v1/tasks/{id}', name: 'v1_task', methods: ['GET'])]
     public function show(int $id): Response
     {
-        return $this->json(['message' => 'Hello world'], Response::HTTP_OK);
+        $task = $this->taskService->fetchById($id);
+
+        if ($task === null) {
+            return $this->json(['message' => 'Task not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->json(['task' => $task], Response::HTTP_OK);
     }
 
     #[Route(path: '/v1/tasks', name: 'v1_tasks', methods: ['POST'])]
-    public function store(): Response
+    public function store(
+        #[MapRequestPayload] Task $task
+    ): Response
     {
-        return $this->json(['message' => 'Hello world'], Response::HTTP_CREATED);
+        try {
+            $task = $this->taskService->create($task);
+        } catch (\Exception $e) {
+            return $this->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->json(['task' => $task], Response::HTTP_CREATED);
     }
 
     #[Route(path: '/v1/tasks/{id}', name: 'v1_task', methods: ['PATCH'])]
-    public function update($id): Response
+    public function update(
+        int $id, #[MapRequestPayload] Task $task
+    ): Response
     {
-        return $this->json(['message' => 'Hello world'], Response::HTTP_OK);
+        try {
+            $this->taskService->update($task);
+        } catch (\Exception $e) {
+            return $this->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->json(['message' => 'Task was successfully updated.'], Response::HTTP_OK);
     }
 
     #[Route(path: '/v1/tasks/{id}', name: 'v1_task', methods: ['PUT'])]
     public function destroy($id): Response
     {
-        return $this->json(['message' => 'Hello world'], Response::HTTP_OK);
+        try {
+            $this->taskService->delete($id);
+        } catch (\Exception $e) {
+            return $this->json(['message' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->json(['message' => 'Task was successfully deleted.'], Response::HTTP_OK);
     }
-
-
 }
--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -4,9 +4,38 @@ namespace App\Service;
 
 use App\Entity\Task;
 use App\Repository\TaskRepository;
-use Doctrine\ORM\EntityManagerInterface;
 
-class TaskService
+readonly class TaskService
 {
-    
+    public function __construct(private TaskRepository $taskRepository)
+    {
+
+    }
+
+    public function fetchAll(): array
+    {
+        return $this->taskRepository->fetchAll();
+    }
+
+    public function fetchById(int $id): ?Task
+    {
+        return $this->taskRepository->fetchById($id);
+    }
+
+    public function create(Task $task): Task
+    {
+        $this->taskRepository->save($task, true);
+        return $task;
+    }
+
+    public function update(Task $task): Task
+    {
+        $this->taskRepository->save($task, true);
+        return $task;
+    }
+
+    public function delete(Task $task): void
+    {
+        $this->taskRepository->remove($task, true);
+    }
 }
